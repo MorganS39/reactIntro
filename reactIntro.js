5b1123dfe91ec1112903e93bcd358cdd353cc695
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-09 10:58:30 -1000

    Use PropTypes to define the props you expect

--- reactIntro.js
+++ reactIntro.js
@@ -244,3 +244,23 @@ class ShoppingCart extends React.Component {
 };
 
 // Use PropTypes to define the props you expect
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+};
+
+Items.propTypes = { quantity: PropTypes.number.isRequired }
+
+Items.defaultProps = {
+  quantity: 0
+};
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Items />
+  }
+};
+
+//
