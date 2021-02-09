a87ff34e34293765dab6fddfff89ed79466f865c
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-09 07:52:45 -1000

    created a const for multiple html elements

--- reactIntro.js
+++ reactIntro.js
@@ -1,1 +1,13 @@
 const JSX = <h1>Hello JSX!</h1>;
+
+const JSX = 
+  <div>
+    <h1>Heading</h1>
+    <p>The paragraph describing the heading</p>
+    <ul>
+      <li>beans</li>
+      <li>turkey</li>
+      <li>some form of cheese</li>
+    </ul>
+  </div>
+
