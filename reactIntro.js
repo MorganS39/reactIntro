8a0d3511f0d031a5f35ec3411f7428cc21e054c1
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-09 08:35:08 -1000

    creating extended classes in react

--- reactIntro.js
+++ reactIntro.js
@@ -37,3 +37,16 @@ const MyComponent = function() {
     );
 };
 
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>Hello React!</h1>
+        </div>
+      );
+    }
+};
+
