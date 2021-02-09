5a8956f318e50d90bba7f25d2fc60edc832955ba
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-09 11:15:27 -1000

    Review proprs using stateless func components

--- reactIntro.js
+++ reactIntro.js
@@ -296,3 +296,26 @@ class ResetPassword extends React.Component {
 };
 
 // Review using props with stateless functional components
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
+const Camper = props => <p>{props.name}</p>;
+
+Camper.defaultProps = {
+  name: "CamperBot"
+};
+
+Camper.propTypes = {
+  name: PropTypes.string.isRequired
+};
+
+//
