991885511340a1dbcb0263cf929484654b661fef
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-09 08:08:26 -1000

    learned how to add comments in jsx

--- reactIntro.js
+++ reactIntro.js
@@ -2,6 +2,7 @@ const JSX = <h1>Hello JSX!</h1>;
 
 const JSX = 
   <div>
+    {/*This JSX code wont work because I don't have node.js installed */}
     <h1>Heading</h1>
     <p>The paragraph describing the heading</p>
     <ul>
