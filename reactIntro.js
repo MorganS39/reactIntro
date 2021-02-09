d25f75e9368d4074de2c50ae172898f680870e63
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-09 08:15:04 -1000

    learned how to add classes to JSX format

--- reactIntro.js
+++ reactIntro.js
@@ -14,3 +14,10 @@ const JSX = (
 );
 
 ReactDOM.render(JSX, document.getElementById('challenge-node'))
+
+const JSX = (
+    <div className="myDiv">
+      <h1>Add a class to this div</h1>
+    </div>
+);
+
