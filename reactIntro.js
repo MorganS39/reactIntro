8e9610b12dd8b5f1a91ef0a79a27b79c2f4de818
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-09 08:58:28 -1000

    Render class component to the DOM

--- reactIntro.js
+++ reactIntro.js
@@ -139,3 +139,19 @@ class TypesOfFood extends React.Component {
   }
 };
 
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        <Fruits />
+        <Vegetables />
+      </div>
+    );
+  }
+};
+ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'));
+
