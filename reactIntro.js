959c7fe3ae88bb6bb16a84ae3e0f1753d2714953
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-09 10:49:47 -1000

    Use default props

--- reactIntro.js
+++ reactIntro.js
@@ -215,4 +215,14 @@ class ToDo extends React.Component {
   }
 };
 
-// 
+// Use default props
+const ShoppingCart = (props) => {
+  return (
+    <div>
+      <h1>Shopping Cart Component</h1>
+    </div>
+  )
+};
+ShoppingCart.defaultProps = { items: 0 };
+
+// Override default props
