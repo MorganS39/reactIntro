4c214827145a31e4a09162a663f9e2f32b85fcef
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-10 13:57:27 -1000

    give sibling elements a unique key attribute

--- reactIntro.js
+++ reactIntro.js
@@ -1076,3 +1076,25 @@ class MyToDoList extends React.Component {
 }
 
 // Give sibling elements a unique key attribute
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((item) => <li key={item}>{item}</li>);
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
+
+// use array.filter() to dynamically filter an array
