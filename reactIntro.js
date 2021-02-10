61a6f3f1c8e30ebb29c5317c4a5a2ead24614fec
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-10 10:32:38 -1000

    set state with this.setState method

--- reactIntro.js
+++ reactIntro.js
@@ -372,4 +372,28 @@ class MyComponent extends React.Component {
   }
 };
 
+// set state with this.setState
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Initial State'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+      name: "React Rocks!"
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+};
+
 //
