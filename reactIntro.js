b87be87b0db9b3da4a35fd49c3d95a76a4680fd8
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-10 13:48:51 -1000

    change inline CSS conditionally based on state

--- reactIntro.js
+++ reactIntro.js
@@ -998,3 +998,35 @@ class GameOfChance extends React.Component {
 }
 
 // Change inline CSS conditionally based on component state
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value })
+  }
+  render() {
+    let inputStyle = {
+      border: '1px solid black'
+    };
+    if (this.state.input.length > 15) {
+      inputStyle.border = '3px solid red';
+    }
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange} />
+      </div>
+    );
+  }
+};
+
+// Use array.map() to dynamically render elements
