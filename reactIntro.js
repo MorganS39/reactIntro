091073da8208daca4dd04346c09575f210410da1
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-10 13:20:23 -1000

    render with an If-Else condition

--- reactIntro.js
+++ reactIntro.js
@@ -849,3 +849,35 @@ class MagicEightBall extends React.Component {
 }
 
 // render with an If-Else condition
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState((state) => ({
+      display: !state.display
+    }));
+  }
+  render() {
+    if (this.state.display) {
+    return (
+       <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         <h1>Displayed!</h1>
+       </div>
+    );
+  } else {
+    return (
+      <div>
+        <button onClick={this.toggleDisplay}>Toggle Display</button>
+      </div>
+    );
+  }
+  }
+};
+
+// use && for a more concise conditional
