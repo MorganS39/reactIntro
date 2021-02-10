c60a7983cdcc44ae727afea4ecce57787e18840c
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-10 10:21:58 -1000

    Render state in the UI

--- reactIntro.js
+++ reactIntro.js
@@ -336,3 +336,20 @@ class StatefulComponent extends React.Component {
 };
 
 // Render state in the user interface
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+};
+
+// Render state in the user interface another way
